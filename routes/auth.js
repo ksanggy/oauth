1986ff641803941df9cc63398ac7092a1004a346
Sangho Kim <sanggy10823@gmail.com>
2023-05-06 23:33:43 -0400

    test commit for console

--- routes/auth.js
+++ routes/auth.js
@@ -27,6 +27,7 @@ router.post('/register', async (req, res) => {
 
   try {
     const savedUser = await user.save();
+    console.log('this is saved user', savedUser);
     res.send({user: user._id});
   } catch (error) {
     res.status(400).send(error);
